c6d14dc52590dae15b6bc8bf07dec343d12b3b59
Julian Nonino <jnonino@users.noreply.github.com>
2023-11-10 13:59:39 +0000

    Update load.py

--- backend/load.py
+++ backend/load.py
@@ -9,13 +9,16 @@ import config
 from db import Database
 
 
+PDF_DIRECTORY="./docs/test"
+
+
 # Langchain document loaders
 # https://python.langchain.com/docs/integrations/document_loaders
 def load_documents_chunks():
     """Loading documents and create chunks from them
     """
     logging.info("Loading documents")
-    loader = PyPDFDirectoryLoader(path=config.PDF_DIRECTORY, recursive=True)
+    loader = PyPDFDirectoryLoader(path=PDF_DIRECTORY, recursive=True)
     documents = loader.load()
 
     logging.info("Splitting documents into chunks")
