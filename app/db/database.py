cadbc65b9a635e7c797e891f33f903c8e1dd1c94
Julian Nonino <noninojulian@gmail.com>
2025-04-01 23:07:31 +0100

    Commit in progress changes

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,13 @@
+from abc import ABC
+
 from langchain_core.vectorstores import VectorStore
 
 # TODO:
 # [ ] Use indexing API: https://python.langchain.com/docs/how_to/indexing/
 
 
-class Database():
+class Database(ABC):
+    """Abstract class for database interaction"""
 
     def __init__(self, vector_store: VectorStore) -> None:
         self.vector_store = vector_store
